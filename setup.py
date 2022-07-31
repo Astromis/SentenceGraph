26863e48da3f5d43d3ec1f7d041f7e1de378b488
Igor Buyanov <buyanov.igor.o@yandex.ru>
2022-07-31 16:23:46 +0300

    [doc] setup version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="sentence_graph",
-    version="0.1.0",
+    version="0.2.0",
     author="Igor Buyanov",
     author_email="buyanov.igor.o@yandex.ru",
     description="A handy wrapper for UDPipe",
